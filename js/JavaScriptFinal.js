1f9f4cb4cf5ed1d3fe6ab026ab479d10aafdb52f
Jordan Jessee <jomaje8@gmail.com>
2016-10-10 11:49:26 -0600

    start game, create charecters

---
+++ js/JavaScriptFinal.js
@@ -0,0 +1,36 @@
+// Generic Fantasy 1
+// 
+
+// list of charecters
+alert("welcome to the wolrd of Generic Fantasy!")
+function Hero (player){
+    health = 100;
+    strength =25;
+    agility = 15;
+    armor = 25;
+}
+
+var Vampire = {
+    health = 12,
+    strength = 50,
+    agility = 40,
+    armor = 10,
+}
+
+var Zombie = {
+    health = 1,
+    strength = 30,
+    agility = 5,
+    armor = 5,
+}
+
+var Harrambe = {
+    health = 60,
+    stregnth = 75,
+    agility = 10,
+    armor = 10,
+}
+
+var mainPlayer = prompt("Welcome Hero, we are in dire need of your help! Please, what shall I call you?")
+
+
