e68078488137c35815376a03998ec412da1822b9
Jordan Jessee <jomaje8@gmail.com>
2016-10-12 11:52:02 -0600

    create charecter

--- js/JavaScriptFinal.js
+++ js/JavaScriptFinal.js
@@ -1,23 +1,77 @@
 // Generic Fantasy 1
 
-// list of charecters
+// Laying ground work for story
 alert("welcome to the wolrd of Generic Fantasy!")
-function Hero (player){
-    health = 100;
-    strength =25;
-    agility = 15;
-    armor = 25;
+alert("Our dear Hero! We are in deep need of your help! Our world has been overrun by monsters who ")
+
+var beginningScenario = (" ")
+
+function randomNumber(range) {
+  "use strict";
+  if (typeof range === "number") {
+    return Math.round(Math.random() * range);
+  }
 }
 
-var Vampire = {
-    health = 12,
-    strength = 50,
-    agility = 40,
-    armor = 10,
+alert(beginningScenarios[randomNumber(beginningScenarios.length - 1)]);
+
+
+// Creation of the Hero
+function Hero () {
+    var name;
+    var speed = 10;
+    var strength = 25;
+    var health = 100;
+    var HeroClass = prompt("Our world tends to test the strengths of our Heros. Who were you before you entered our world? (Choose from:McDonalds Worker, Prison Gaurd, Professor)").toLowerCase();
+    var weapon;
+    }
+time.limit
+
+
+if (!Hero.name) {
+  Hero.name =prompt("No name? Are you sure? If you don't answer I will be forced to name you myself...Last chance. What is your name?");
+
+if (!Hero.name) {
+   alert("Well I told you I'd do it. Sir Buttrash it is then.");
+    character.name = "Sir Buttrash";
+  }
+}
+
+if (!Hero.name) {
+  character.name =prompt("No name? Are you sure? If you don't answer I will be forced to name you myself...Last chance. What is your name?");
+
+if (!Hero.name) {
+   alert("Well I told you I'd do it. Sir Buttrash it is then.");
+    character.name = "Sir Buttrash";
+  }
+}
+
+if (Hero.HeroClass === "McDonalds Worker") {
+  Hero.speed = 4;
+}
+
+if (Hero.HeroClass === "Prison Gaurd") {
+  character.strength = 45;
+}
+
+if (Hero.HeroClass === "Professor") {
+  character.health = 130;
+}
+
+var choice =prompt("Our Hero" + Hero.HeroClass + " " + Hero.name + "does this ").toLowerCase();
+
+
+//list of monsters
+function Monster () {
+    var name;
+    var health = 50;
+    var strength = 50;
+    var agility = 40;
+    var armor = 10;
 }
 
 var Zombie = {
-    health = 1,
+    health = 10,
     strength = 30,
     agility = 5,
     armor = 5,
@@ -30,6 +84,10 @@ var Harrambe = {
     armor = 10,
 }
 
-var mainPlayer = prompt("Welcome Hero, we are in dire need of your help! Please, what shall I call you?")
+
+//List of items
+
+Hero.weapon =(CD, wrench, sword, apple, ladle, machette)
+
 
 
